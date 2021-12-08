2ab54f8a21ae7efdaee55bd40f70e9c8cb3656d0
mejerr <georgy_90@abv.bg~>
2021-12-08 13:09:55 +0200

    Download images and upload new ones

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,32 +1,56 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import france from '../assets/france.jpg'
 import china from '../assets/china.jpg'
 import { saveAs } from 'file-saver'
 
 const imagesArray = [{
   imageUrl: france,
-  exportUrl: 'france'
+  exportUrl: 'france.jpg'
 }, {
   imageUrl: china,
-  exportUrl: 'china'
-}
-];
+  exportUrl: 'china.jpg'
+}];
+
 
 const Gallery = () => {
-    const downloadImage = useCallback((image) => {
-      saveAs('image_url', `${image}.jpg`)
-    }, []);
+  const [images, setImages] = useState(
+    imagesArray.map(({ imageUrl, exportUrl }) => ({
+      imageUrl,
+      exportUrl
+    }))
+  );
+
+  const downloadImage = useCallback((image) => {
+    saveAs('image_url', `${image}`)
+  }, []);
+
+  const onImageChange = event => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+
+      setImages([...images, {
+        imageUrl: URL.createObjectURL(img),
+        exportUrl: img.name
+      }]);
+    }
+  };
 
   return (
     <div className="gallery-container content">
-      {imagesArray.map(({ imageUrl, exportUrl }) => (
-        <div className="gallery-image" key={imageUrl} style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: 'cover'}}>
+      {images.map((image) => (
+        <div className="gallery-image" key={image.imageUrl} style={{ backgroundImage: `url(${image.imageUrl})`, backgroundSize: 'cover'}}>
           <div className="gallery-image-buttons">
             <div className="image-open-btn button"><i className="fas fa-info-circle"></i></div>
-            <div className="image-download-btn button" onClick={() => downloadImage(exportUrl)}><i className="fas fa-arrow-circle-down"></i></div>
+            <div className="image-download-btn button" onClick={() => downloadImage(image.exportUrl)}><i className="fas fa-arrow-circle-down"></i></div>
           </div>
         </div>
       ))}
+      <div className="upload-btn">
+        <div className="upload-icon">
+          <i className="fas fa-upload"></i>
+          <input type="file" className="upload-button" onChange={onImageChange}/>
+        </div>
+      </div>
     </div>
   )
 };
