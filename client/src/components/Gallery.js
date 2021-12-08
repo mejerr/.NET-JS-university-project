f4b70210399f4b19f9fe3aecb5d14f77a9c0f3e2
mejerr <georgy_90@abv.bg~>
2021-12-08 10:18:05 +0200

    Download image npm packet installed and added

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,16 +1,29 @@
-import { Fragment } from 'react';
+import { useCallback } from 'react';
 import france from '../assets/france.jpg'
+import china from '../assets/china.jpg'
+import { saveAs } from 'file-saver'
 
-const imagesArray = [france, france, france, france, france, france];
+const imagesArray = [{
+  imageUrl: france,
+  exportUrl: 'france'
+}, {
+  imageUrl: china,
+  exportUrl: 'china'
+}
+];
 
 const Gallery = () => {
+    const downloadImage = useCallback((image) => {
+      saveAs('image_url', `${image}.jpg`)
+    }, []);
+
   return (
     <div className="gallery-container content">
-      {imagesArray.map(el => (
-        <div className="gallery-image" key={el} style={{ backgroundImage: `url(${el})`, backgroundSize: 'cover'}}>
+      {imagesArray.map(({ imageUrl, exportUrl }) => (
+        <div className="gallery-image" key={imageUrl} style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: 'cover'}}>
           <div className="gallery-image-buttons">
             <div className="image-open-btn button"><i className="fas fa-info-circle"></i></div>
-            <div className="image-download-btn button"><i className="fas fa-arrow-circle-down"></i></div>
+            <div className="image-download-btn button" onClick={() => downloadImage(exportUrl)}><i className="fas fa-arrow-circle-down"></i></div>
           </div>
         </div>
       ))}
