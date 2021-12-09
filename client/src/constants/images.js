9722d9f1c998bebbbc4e9668f9d2263db074d759
mejerr <georgy_90@abv.bg>
2021-12-09 17:41:59 +0200

    Default Images added

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,14 +1,38 @@
-import france from '../assets/france.jpg';
 import china from '../assets/china.jpg';
+import girl from '../assets/girl.png';
+import boy from '../assets/boy.png';
+import horse from '../assets/horse.png';
+import flowers from '../assets/flowers.png';
+import eyes from '../assets/eyes.png';
 
 export const imagesArray = [{
   id: 0,
-  imageUrl: france,
-  exportUrl: 'france.jpg'
+  imageUrl: girl,
+  exportUrl: 'girl.png'
 }, {
   id: 1,
   imageUrl: china,
   exportUrl: 'china.jpg'
+},
+{
+  id: 2,
+  imageUrl: boy,
+  exportUrl: 'boy.png'
+},
+{
+  id: 3,
+  imageUrl: horse,
+  exportUrl: 'horse.png'
+},
+{
+  id: 4,
+  imageUrl: flowers,
+  exportUrl: 'flowers.png'
+},
+{
+  id: 5,
+  imageUrl: eyes,
+  exportUrl: 'eyes.png'
 }];
 
 export const ADD_IMAGE = 'ADD_IMAGE';
