ee479872ec6d995dbdc9c826da1d6d6dc3a0f1dd
mejerr <georgy_90@abv.bg~>
2022-02-07 01:09:43 +0200

    Refactor

--- server2/WebApi/Data/ApiDbContext.cs
+++ server2/WebApi/Data/ApiDbContext.cs
@@ -16,28 +16,5 @@ namespace WebApi.Data
 
         }
         public DbSet<Comment> Comments { get; set; }
-        public DbSet<Image> Images { get; set; }
-        /* public DbSet<Album> Albums { get; set; }*/
-
-        // Seed script
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Song>().HasData(
-        //        new Song
-        //        {
-        //            Id = 1,
-        //            Title = "Willow",
-        //            Language = "Spanish",
-        //            Duration = "4.35"
-        //        },
-        //         new Song
-        //         {
-        //             Id = 2,
-        //             Title = "Despacito",
-        //             Language = "Spanish",
-        //             Duration = "3.35"
-        //         }
-        //        );
-        //}
     }
 }
